cebbe41425eb6046d21c28f2d0860d4c175c3161
Taha <tahaguler95@hotmail.com>
2021-01-15 17:48:39 -0500

    Created Angular. Added ssl cert. Connected to api.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'MyExpense';
+  users: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() { // this is executed right after constructor.
+    this.getUsers();
+  }
+
+  getUsers() { //   https://localhost:44313
+    this.http.get('http://localhost:55849/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
